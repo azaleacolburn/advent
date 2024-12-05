2a5d2b504d8681d3c8af92676f0989bb85dcb994
Azalea Colburn <azaleacolburn@gmail.com>
2024-12-05 08:24:40 -0800

    wip(day4): rotate matrix 90 deg

--- 2024/src/day4.rs
+++ 2024/src/day4.rs
@@ -2,8 +2,44 @@ use std::fs;
 
 pub fn main() {
     let buff = fs::read_to_string("./inputs/day4.txt").unwrap();
-    let matrix = buff
+    let regex = regex::Regex::new(r"XMAS").unwrap();
+    let mut matches = vec![];
+    matches.push(regex.find_iter(&buff));
+    let mut matrix = buff
+        .trim()
         .split('\n')
-        .map(|row| row.split_whitespace().collect::<Vec<&str>>())
+        .map(|row| row.split("").collect::<Vec<&str>>())
         .collect::<Vec<Vec<&str>>>();
+    rotate_90(&mut matrix);
+    matches.push(regex.find_iter(&buff));
+    rotate_90(&mut matrix);
+    matches.push(regex.find_iter(&buff));
+    rotate_90(&mut matrix);
+    matches.push(regex.find_iter(&buff));
+}
+
+fn rotate_45(matrix: &mut Vec<Vec<&str>>) {
+    let len = matrix.len();
+    matrix.iter_mut().for_each(|row| row.reverse());
+
+    for i in 0..len {
+        for j in i + 1..len {
+            let temp = matrix[i][j];
+            matrix[i][j] = matrix[j][i];
+            matrix[j][i] = temp;
+        }
+    }
+}
+
+fn rotate_90(matrix: &mut Vec<Vec<&str>>) {
+    let len = matrix.len();
+    matrix.iter_mut().for_each(|row| row.reverse());
+
+    for i in 0..len {
+        for j in i + 1..len {
+            let temp = matrix[i][j];
+            matrix[i][j] = matrix[j][i];
+            matrix[j][i] = temp;
+        }
+    }
 }
