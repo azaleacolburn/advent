9404cadd514f425bf3444eeec21591089f58d9a0
Azalea Colburn <azalea.colburn@autodesk.com>
2024-12-05 18:36:48 -0800

    wip(day4): rotation works

--- 2024/src/day4.rs
+++ 2024/src/day4.rs
@@ -1,42 +1,26 @@
 use std::fs;
 
 pub fn main() {
-    // let mut buff = fs::read_to_string("./inputs/day4.txt").unwrap();
-    // let mut count = 0;
-    // count += count_matches(&buff);
-    // let mut matrix: Vec<Vec<char>> = buff
-    //     .trim()
-    //     .split('\n')
-    //     .map(|row| row.trim().chars().collect::<Vec<char>>())
-    //     .collect::<Vec<Vec<char>>>();
-    // for i in 0..8 {
-    //     matrix = rotate_45(&matrix, i);
-    //     buff = matrix
-    //         .iter()
-    //         .map(|row| row.iter().collect::<String>())
-    //         .collect::<Vec<String>>()
-    //         .join("\n");
-    //     count += count_matches(&buff);
-    // }
-    //
-    // println!("{count}");
-    let s = vec![
-        vec!['A', 'B', 'C', 'D'],
-        vec!['E', 'F', 'G', 'H'],
-        vec!['I', 'J', 'K', 'L'],
-        vec!['M', 'N', 'O', 'P'],
-    ];
-    let g = rotate_45(&s, 0);
-    let k = rotate_45(&g, 1);
-    let h = rotate_45(&k, 2);
-    let i = rotate_45(&h, 3);
-    let l = rotate_45(&i, 4);
-    println!("{:?}", s);
-    println!("{:?}", matrix_format(&g));
-    println!("{:?}", matrix_format(&k));
-    println!("{:?}", matrix_format(&h));
-    println!("{:?}", matrix_format(&i));
-    println!("{:?}", matrix_format(&l));
+    let buff = fs::read_to_string("./inputs/day4.txt").unwrap();
+    let mut count = 0;
+    let mut matrix: Vec<Vec<char>> = buff
+        .trim()
+        .split('\n')
+        .map(|row| row.trim().chars().collect::<Vec<char>>())
+        .collect::<Vec<Vec<char>>>();
+    count += count_matches(
+        matrix
+            .iter()
+            .map(|row| row.iter().collect::<String>())
+            .collect::<Vec<String>>(),
+    );
+    for i in 0..8 {
+        matrix = rotate_45(&matrix, i);
+        let buff = matrix_format(&matrix);
+        count += count_matches(buff);
+    }
+
+    println!("{count}");
 }
 
 fn rotate_45<'a>(matrix: &Vec<Vec<char>>, rots: usize) -> Vec<Vec<char>> {
@@ -44,7 +28,6 @@ fn rotate_45<'a>(matrix: &Vec<Vec<char>>, rots: usize) -> Vec<Vec<char>> {
     let mut size = old_size;
     size = size * 2 - 1;
     let mut new_matrix = vec![vec![' '; size]; size];
-    println!("{} {}", size, size);
 
     for i in 0..old_size {
         for j in 0..old_size {
@@ -63,6 +46,7 @@ fn rotate_45<'a>(matrix: &Vec<Vec<char>>, rots: usize) -> Vec<Vec<char>> {
                     .filter(|c| *c != ' ')
                     .collect::<Vec<char>>()
             })
+            .filter(|row| !row.is_empty())
             .collect()
     } else {
         new_matrix
@@ -77,24 +61,13 @@ fn matrix_format(matrix: &Vec<Vec<char>>) -> Vec<String> {
         .collect::<Vec<String>>()
 }
 
-fn count_matches(buff: &str) -> usize {
-    regex::Regex::new(r"X M A S")
-        .unwrap()
-        .find_iter(buff)
-        .map(|m| m.as_str().to_string())
-        .collect::<Vec<String>>()
-        .len()
-}
-
-fn rotate_90(matrix: &mut Vec<Vec<&str>>) {
-    let len = matrix.len();
-    matrix.iter_mut().for_each(|row| row.reverse());
-
-    for i in 0..len {
-        for j in i + 1..len {
-            let temp = matrix[i][j];
-            matrix[i][j] = matrix[j][i];
-            matrix[j][i] = temp;
-        }
-    }
+fn count_matches(buff: Vec<String>) -> usize {
+    buff.iter().fold(0, |acc, row| {
+        acc + regex::Regex::new(r"XMAS")
+            .unwrap()
+            .find_iter(row)
+            .map(|m| m.as_str().to_string())
+            .collect::<Vec<String>>()
+            .len()
+    })
 }
